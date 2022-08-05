9a7f43bfcc6d7b7cdfc94ba2c76530e5cf74fda7
tuyucheng <tuyucheng2000@163.com>
2022-08-05 22:51:45 +0800

    config file

---
+++ testing-in-action-junit-5-basics/src/main/resources/jdbc/schema.sql
@@ -0,0 +1,7 @@
+create table employee
+(
+    id         int NOT NULL PRIMARY KEY,
+    first_name varchar(255),
+    last_name  varchar(255),
+    address    varchar(255)
+);
---
+++ testing-in-action-junit-5-basics/src/main/resources/jdbc/test-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO employee
+VALUES (1, 'James', 'Gosling', 'Canada');
+INSERT INTO employee
+VALUES (2, 'Donald', 'Knuth', 'USA');
+INSERT INTO employee
+VALUES (3, 'Linus', 'Torvalds', 'Finland');
+INSERT INTO employee
+VALUES (4, 'Dennis', 'Ritchie', 'USA');
