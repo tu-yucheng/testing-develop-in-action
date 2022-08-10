87be03f394382d49780fd2374d9d3b4adcb5e287
tuyucheng <tuyucheng2000@163.com>
2022-08-10 21:42:56 +0800

    add code for "A Quick Guide to @DirtiesContext" case

---
+++ testing-in-action-junit-5-spring-1/src/main/java/cn/tuyucheng/taketoday/dirtiescontext/User.java
@@ -0,0 +1,6 @@
+package cn.tuyucheng.taketoday.dirtiescontext;
+
+public class User {
+    String firstName;
+    String lastName;
+}
---
+++ testing-in-action-junit-5-spring-1/src/main/java/cn/tuyucheng/taketoday/dirtiescontext/UserCache.java
@@ -0,0 +1,25 @@
+package cn.tuyucheng.taketoday.dirtiescontext;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class UserCache {
+    @Getter
+    private final Set<String> userList = new HashSet<>();
+
+    public void addUser(String user) {
+        userList.add(user);
+    }
+
+    public boolean removeUser(String user) {
+        return userList.remove(user);
+    }
+
+    public void printUserList(String message) {
+        System.out.println(message + ": " + userList);
+    }
+}
