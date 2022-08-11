c6793ada03db018ecd6e31a272628df0ff26d7f8
tuyucheng <tuyucheng2000@163.com>
2022-08-11 22:52:36 +0800

    add code for "Spring 5 Testing with @EnabledIf Annotation" case

---
+++ testing-in-action-junit-5-spring-2/src/test/java/cn/tuyucheng/taketoday/enabledif/Spring5EnabledAnnotationIntegrationTest.java
@@ -0,0 +1,50 @@
+package cn.tuyucheng.taketoday.enabledif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.DisabledIf;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringJUnitConfig(Spring5EnabledAnnotationIntegrationTest.Config.class)
+@TestPropertySource(properties = {"tests.enabled=true"})
+public class Spring5EnabledAnnotationIntegrationTest {
+
+    @EnabledIf("true")
+    @Test
+    void givenEnabledIfLiteral_WhenTrue_ThenTestExecuted() {
+        assertTrue(true);
+    }
+
+    @EnabledIf(expression = "${tests.enabled}", loadContext = true)
+    @Test
+    void givenEnabledIfExpression_WhenTrue_ThenTestExecuted() {
+        assertTrue(true);
+    }
+
+    @EnabledIf("#{systemProperties['java.version'].startsWith('17')}")
+    @Test
+    void givenEnabledIfSpel_WhenTrue_ThenTestExecuted() {
+        assertTrue(true);
+    }
+
+    @EnabledOnJava17
+    @Test
+    void givenEnabledOnJava8_WhenTrue_ThenTestExecuted() {
+        assertTrue(true);
+    }
+
+    @DisabledIf("#{systemProperties['java.version'].startsWith('17')}")
+    @Test
+    void givenDisabledIf_WhenTrue_ThenTestNotExecuted() {
+        assertTrue(true);
+    }
+
+    @Configuration
+    static class Config {
+
+    }
+}
